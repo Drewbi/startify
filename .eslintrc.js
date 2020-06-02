548deaa74045b8de8fa24b662152bf34bae1a4c4
Drew Alexander <22423862@student.uwa.edu.au>
2020-06-02 15:59:00 +0800

    Set up basic project (#3)
    * Created boilerplate using create next-app
    
    * Removed api boilerplate
    
    * Removed semicolon
    
    * Added eslint and prettier
    
    * Configured eslint and prettier with husky pre-commit
    
    * Added materialUI
    
    * Changed home page to tsx
    
    * Added next config with typescript

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  globals: {
+    React: 'writable',
+  },
+};
