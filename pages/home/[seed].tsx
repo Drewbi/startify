506e3e1fa37ecc1604fe2d1d02e7371566b0be16
Nisarag <nisaragb@gmail.com>
2020-06-05 10:18:57 +1200

    fixing warnings

--- pages/home/[seed].tsx
+++ pages/home/[seed].tsx
@@ -12,27 +12,25 @@ import {
 } from '../../components';
 import { getFakeData } from '../../core/faker';
 
-export default function Home() {
+const Home: React.FC = () => {
   const { seed } = routeParams();
   const fakeData = getFakeData(seed);
 
-  const secondRowData = [
-    {
+  if (!fakeData) {
+    return <p> Loading </p>;
+  }
+
+  const getSecondRowData = () => {
+    const { companyBs } = fakeData;
+    if (!companyBs) return [];
+
+    return companyBs.map((data) => ({
       imageSrc: '/bear.svg',
-      title: fakeData.companyBs[0].title,
-      bodyText: fakeData.companyBs[0].bodyText.substr(0, 200),
-    },
-    {
-      imageSrc: '/notes.svg',
-      title: fakeData.companyBs[1].title,
-      bodyText: fakeData.companyBs[1].bodyText.substr(0, 200),
-    },
-    {
-      imageSrc: '/stats.svg',
-      title: fakeData.companyBs[2].title,
-      bodyText: fakeData.companyBs[2].bodyText.substr(0, 200),
-    },
-  ];
+      title: data?.title,
+      bodyText: data?.bodyText.substring(0, 200),
+    }));
+  };
+
   return (
     <div className="container">
       <Head>
@@ -48,23 +46,25 @@ export default function Home() {
       <Hero />
       <FirstRow
         titleText={fakeData.companyCatchPhrase[0].title}
-        firstText={fakeData.companyCatchPhrase[0].bodyText.substr(0, 100)}
-        secondText={fakeData.companyCatchPhrase[2].bodyText}
+        firstText={fakeData.companyCatchPhrase[0].bodyText?.substr(0, 100)}
+        secondText={fakeData?.companyCatchPhrase[2].bodyText}
         imageSrc="/stats.svg"
       />
-      <SecondRow data={secondRowData} />
+      <SecondRow data={getSecondRowData()} />
       <ThirdRow
-        title={fakeData.companyCatchPhrase[1].title}
-        bodyText={fakeData.companyCatchPhrase[1].bodyText}
+        title={fakeData?.companyCatchPhrase[1].title}
+        bodyText={fakeData?.companyCatchPhrase[1].bodyText}
         imageSrc="https://unsplash.com/photos/Ua-agENjmI4/download"
       />
       <Footer
         companyName={fakeData.companyName}
-        quote={fakeData.companyCatchPhrase[2].title}
+        quote={fakeData?.companyCatchPhrase[2].title}
         address={fakeData.address}
         phoneNumber={fakeData.phoneNumber}
         person={fakeData.person}
       />
     </div>
   );
-}
+};
+
+export default Home;
