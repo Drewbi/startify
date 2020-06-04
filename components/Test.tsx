cdac0ee72e06085db9c16cd68e7873f05e94c28b
Drew Alexander <22423862@student.uwa.edu.au>
2020-06-05 02:58:21 +0800

    Added source svg files (#23)
    * Added source svg files
    
    * Basic illustration component

--- components/Test.tsx
+++ components/Test.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { FakeContext } from './FakeContext';
+import Illustration from './Illustration';
 
 const Test: React.FC = () => {
   const { fakeData } = useContext(FakeContext) as {
@@ -8,6 +9,7 @@ const Test: React.FC = () => {
   return (
     <React.Fragment>
       <p>{fakeData?.name}</p>
+      <Illustration name="undraw_code_inspection_bdl7.svg" colour="#ff3388" />
     </React.Fragment>
   );
 };
