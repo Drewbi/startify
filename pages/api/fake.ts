77967b48f2deb0318ef84da1f0d6aa5e364fe699
Drewbi <drewalexander986@gmail.com>
2020-06-05 18:17:05 +0800

    Removed api routes

--- pages/api/fake.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getFakeData } from '../../core/faker';
-
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  const fakeData = getFakeData(123);
-  res.status(200).json(fakeData);
-};
